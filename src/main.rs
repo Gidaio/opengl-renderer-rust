2963a6438e22a28a5b4c88b346ca9b059d0fbb05
Gidaio <killshade@gmail.com>
2019-03-26 22:35:13 -0600

    Textures finally work!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate gl;
 extern crate glfw;
+extern crate image;
 
 use gl::types::*;
 use glfw::{ Context };
+use image::GenericImageView;
 use std::ffi::CString;
 
 
@@ -73,10 +75,11 @@ fn main() {
     }
 
     // Make a VBO for our vertices.
-    let vertices: [f32; 18] = [
-        0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-        0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+    let vertices: [f32; 32] = [
+        0.5, 0.5, 0.0,    1.0, 0.0, 0.0,  1.0, 0.0,
+        -0.5, 0.5, 0.0,   1.0, 1.0, 0.0,  0.0, 0.0,
+        -0.5, -0.5, 0.0,  0.0, 0.0, 1.0,  0.0, 1.0,
+        0.5, -0.5, 0.0,   0.0, 1.0, 0.0,  1.0, 1.0
     ];
 
     let mut vbo = 0;
@@ -91,16 +94,47 @@ fn main() {
         );
     }
 
+    let triangles: [i32; 6] = [
+        0, 1, 2,
+        2, 3, 0
+    ];
+
+    let mut ebo = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut ebo);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            std::mem::size_of_val(&triangles) as isize,
+            triangles.as_ptr() as *const _,
+            gl::STATIC_DRAW
+        );
+    }
+
     // Make the vertex attribute pointers.
-    create_vertex_attribute_array::<f32>(0, 3, 6, 0);
-    create_vertex_attribute_array::<f32>(1, 3, 6, 3);
+    create_vertex_attribute_array::<f32>(0, 3, 8, 0);
+    create_vertex_attribute_array::<f32>(1, 3, 8, 3);
+    create_vertex_attribute_array::<f32>(2, 2, 8, 6);
+
+    let _wall_texture = create_texture("./src/wall.jpg", gl::TEXTURE0, gl::RGB);
+    let _face_texture = create_texture("./src/awesomeface.png", gl::TEXTURE1, gl::RGBA);
+    let uniform_0_name = CString::new("texture1").unwrap();
+    let uniform_1_name = CString::new("texture2").unwrap();
+    unsafe {
+        let uniform_0_location = gl::GetUniformLocation(shader_program, uniform_0_name.as_ptr());
+        let uniform_1_location = gl::GetUniformLocation(shader_program, uniform_1_name.as_ptr());
+        gl::Uniform1i(uniform_0_location, 0);
+        gl::Uniform1i(uniform_1_location, 1);
+    }
 
     // Main loop!
     while !window.should_close() {
         // Do rendering stuff.
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0 as *const _);
         }
         window.swap_buffers();
 
@@ -180,4 +214,39 @@ fn create_vertex_attribute_array<T: HasOpenGLType>(index: u32, size: i32, stride
         );
         gl::EnableVertexAttribArray(index);
     }
+}
+
+fn create_texture(path: &'static str, texture_spot: u32, pixel_type: u32) -> u32 {
+    // Load up the image.
+    let image_obj = image::open(path).unwrap();
+    let (image_width, image_height) = image_obj.dimensions();
+    let image_data = image_obj.raw_pixels();
+
+    // Load the texture.
+    let mut texture = 0;
+    unsafe {
+        gl::GenTextures(1, &mut texture);
+        gl::ActiveTexture(texture_spot);
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            gl::RGB as i32,
+            image_width as i32,
+            image_height as i32,
+            0,
+            pixel_type,
+            gl::UNSIGNED_BYTE,
+            image_data.as_ptr() as *const _
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+    }
+
+    return texture;
 }
